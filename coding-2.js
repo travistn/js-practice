681f05765f01ed2d01b6f26e49c71fa5caf48952
travistn <travisn714@gmail.com>
2023-01-26 17:10:16 -0800

    first commit

--- coding-2.js
+++ coding-2.js
@@ -22,6 +22,6 @@ function countUniqueValues(arr) {
   return i + 1;
 }
 
-// console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
-// console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
