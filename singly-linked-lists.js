61f0f832335ceaad6d0fc8050911d5efb9c15fe2
travistn <travisn714@gmail.com>
2023-01-31 13:08:41 -0800

    Added get method

--- singly-linked-lists.js
+++ singly-linked-lists.js
@@ -1,79 +1,91 @@
 // piece of data - val
 //reference to next node - next
 
-class Node{
-  constructor(val){
-      this.val = val;
-      this.next = null;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-class SinglyLinkedList{
-  constructor(){
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
-  push(val){
-    const newNode = new Node(val)
+  push(val) {
+    const newNode = new Node(val);
 
     if (!this.head) {
-      this.head = newNode
-      this.tail = this.head
+      this.head = newNode;
+      this.tail = this.head;
     } else {
-      this.tail.next = newNode
-      this.tail = newNode
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.length++
-    return this
+    this.length++;
+    return this;
   }
   pop() {
-    if (!this.head) return undefined
+    if (!this.head) return undefined;
 
-    let current = this.head
-    let newTail = current
+    let current = this.head;
+    let newTail = current;
 
     while (current.next) {
       if (current.next) {
-        newTail = current
+        newTail = current;
       }
-      current = current.next
+      current = current.next;
     }
-    this.tail = newTail
-    this.tail.next = null
-    this.length--
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
 
-    if(this.length === 0) {
+    if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
     return current;
   }
   shift() {
-    if (!this.head) return undefined
-    let currentHead = this.head
-    this.head = currentHead.next
-    this.length--
-    return currentHead
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    return currentHead;
   }
   unshift(val) {
-    const newNode = new Node(val)
+    const newNode = new Node(val);
 
     if (!this.head) {
-      this.head = newNode
-      this.tail = this.head
+      this.head = newNode;
+      this.tail = this.head;
     } else {
-      newNode.next = this.head
-      this.head = newNode
+      newNode.next = this.head;
+      this.head = newNode;
     }
-    this.length++
-    return this
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
   }
 }
 
-var list = new SinglyLinkedList()
-list.push("HELLO")
-list.push("GOODBYE")
-list.push("!")
+var list = new SinglyLinkedList();
+list.push('HELLO');
+list.push('GOODBYE');
+list.push('!');
 
-console.log(list)
+console.log(list);
