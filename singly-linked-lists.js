3a590d96957c964fc272ca78b842e416cb3eec18
Travis Nguyen <travisn714@gmail.com>
2023-01-31 00:15:52 -0800

    Added a pop method

--- singly-linked-lists.js
+++ singly-linked-lists.js
@@ -27,8 +27,31 @@ class SinglyLinkedList{
     this.length++
     return this
   }
+  pop() {
+    if (!this.head) return undefined
+
+    let current = this.head
+    let newTail = current
+
+    while (current.next) {
+      if (current.next) {
+        newTail = current
+      }
+      current = current.next
+    }
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+
+    if(this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 var list = new SinglyLinkedList()
 list.push("HELLO")
+list.push("GOODBYE")
 list.push("GOODBYE")
