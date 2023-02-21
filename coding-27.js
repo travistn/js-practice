98d2acdf5f00553ebcb3569cc95c6f2de0be3df4
travistn <travisn714@gmail.com>
2023-02-21 15:05:52 -0800

    first commit

---
+++ coding-27.js
@@ -0,0 +1,76 @@
+/*
+DLL unshift - Exercise
+Implement the following on the DoublyLinkedList class
+
+unshift
+
+This function should accept a value and add a node to the beginning of the DoublyLinkedList with the given value. It should return the DoublyLinkedList.
+
+Examples
+
+var doublyLinkedList = new DoublyLinkedList;
+doublyLinkedList.unshift(5); // doublyLinkedList
+doublyLinkedList.length; // 1
+doublyLinkedList.head.val; // 5
+doublyLinkedList.tail.val; // 5
+doublyLinkedList.unshift(10); doublyLinkedList 
+doublyLinkedList.length; // 2
+doublyLinkedList.head.val; // 10
+doublyLinkedList.head.next.val; // 5
+doublyLinkedList.tail.val; // 5
+doublyLinkedList.unshift(15); doublyLinkedList
+doublyLinkedList.length; // 3
+doublyLinkedList.head.val; // 15
+doublyLinkedList.tail.val; // 5
+doublyLinkedList.head.next.next.val; // 5
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.push(5);
+doublyLinkedList.unshift(5);
+
+console.log(doublyLinkedList);
